39022361371a36c9a98ee7b6a0cc6c69f68645d3
dishargavali9699-code <dishargavali9699@gmail.com>
2026-07-24 19:11:51 +0530

    Add files via upload

---
+++ 01-Hello-World/02-Variables/user_input_multiplication.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+void main()
+{
+    int a;
+    int b;
+    
+    printf("Enter Two Number:");
+    scanf("%d",&a);
+    scanf("%d",&b);
+    int multiplication = a * b;
+    printf("multiplication = %d",multiplication);
+
+    
+}
